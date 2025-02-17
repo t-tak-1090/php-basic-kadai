f2079b87b1cff284c9dd1df0b00c7af093ab4889
t-tak-1090 <130780527+t-tak-1090@users.noreply.github.com>
2025-02-17 17:59:29 +0900

    kadai_007

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p>
+        <?php
+            $data = ['name' => 'onion', 'price' => 200, 'weight' => 160];
+            print_r($data);
+        ?>
+    </p>
+</body>
+</html>
